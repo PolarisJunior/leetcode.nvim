0c84c2034b5e424bbfd4624acc896b926b0bda2f
PJ Kumlue <pj.kumlue@gmail.com>
2025-11-22 20:14:40 -0800

    Add basic support for git

--- lua/leetcode/runner/init.lua
+++ lua/leetcode/runner/init.lua
@@ -4,6 +4,9 @@ local log = require("leetcode.logger")
 local interpreter = require("leetcode.api.interpreter")
 local config = require("leetcode.config")
 local Judge = require("leetcode.logger.spinner.judge")
+local utils = require("leetcode.utils")
+local Job = require("plenary.job")
+local Path = require("plenary.path")
 
 ---@type Path
 local leetbody = config.storage.cache:joinpath("body")
@@ -34,6 +37,55 @@ Runner.stop = function()
     Runner.running = false
 end
 
+local GIT_DIRECTORY = "/home/pjkumlue/leetcode"
+
+local function get_extension(lang)
+    return "." .. utils.get_lang(lang).ft
+end
+
+---@param question lc.ui.Question
+---@param source string
+local function run_git_handler(question, source)
+    local title_slug = question.q.title_slug
+    local lang = question.lang
+    local root = Path:new(GIT_DIRECTORY)
+    local ext = get_extension(lang)
+
+    local path = root:joinpath(lang)
+    if not path:exists() then
+        path:mkdir()
+    end
+
+    path = path:joinpath(question.q.difficulty)
+    if not path:exists() then
+        path:mkdir()
+    end
+
+    local file = path:joinpath(question.q.id .. "-" .. title_slug .. ext)
+    file:write(source, "w", nil)
+
+    local result, code = Job:new({
+        command = "git",
+        args = { "add", "-A" },
+        cwd = GIT_DIRECTORY,
+    }):sync()
+
+    local message = "Updated " .. title_slug .. " in " .. lang
+    result, code = Job:new({
+        command = "git",
+        args = { "commit", "-m", message },
+        cwd = GIT_DIRECTORY,
+    }):sync()
+
+    result, code = Job:new({
+        command = "git",
+        args = { "push" },
+        cwd = GIT_DIRECTORY,
+    }):sync()
+
+    vim.notify("Finished uploading submission to git", vim.log.levels.INFO)
+end
+
 function Runner:handle(submit)
     Runner.running = true
     local question = self.question
@@ -53,6 +105,7 @@ function Runner:handle(submit)
 
         if item then
             if item._.success then
+                run_git_handler(question, body.typed_code)
                 judge:success(item.status_msg)
             else
                 judge:error(item.status_msg)
